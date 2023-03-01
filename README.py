b09b38f6b750713146c4ed09625ed8c180be64c3
CarlitosAlex <110439283+CarlitosAlex@users.noreply.github.com>
2023-03-01 12:15:28 -0300

    Add files via upload

---
+++ README.py
@@ -0,0 +1,8 @@
+PROYECTO INDIVIDUAL Nº2
+Data Analytics
+
+El presente trabajo consistió en analizar el índice Standard & Poor’s 500 Index, donde me toco asumir el Rol de “Analista de datos”, para explicar a un cliente en este caso representantes de una empresa, como funciona el mercado Bursatil y recomendar oportunidades de inversión, buscando la información relevante en Yahoo Finance, descargar dicha información en datasets para realizar el proceso de EDA y ETL y generar dataframes con los que trabajar en powerbi; para finalmente analizar los dato y entregar un reporte.
+Además, se preparó un Storytelling, donde le contamos al cliente como funciona el mercado bursátil con ejemplos didácticos y cuales serían las recomendaciones de inversión de una manera clara, evitando el lenguaje técnico de tal manera que cualquier persona entienda el tema.
+Fuente de datos
+•	Los archivos con los datos se encuentran en la carpeta ROE.
+
